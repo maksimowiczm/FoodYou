64b14735fcfc554110ea1c55a2256249ef167d12
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-01-14 18:04:19 +0100

    initial commit

---
+++ app/src/main/java/com/maksimowiczm/foodyou/infrastructure/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.maksimowiczm.foodyou.infrastructure
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.maksimowiczm.foodyou.ui.FoodYouApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodYouApp()
+        }
+    }
+}
