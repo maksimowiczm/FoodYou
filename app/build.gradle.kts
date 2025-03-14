aefc524f9e61029a381d09e10d6e6c65d4dd331d
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-14 14:50:23 +0100

    make it compile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,95 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.room)
+    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    compilerOptions {
+        freeCompilerArgs.add("-Xwhen-guards")
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+
+            implementation(libs.accompanist.permissions)
+
+            // Zxing
+            implementation(libs.zxing.android.embedded)
+
+            // Koin
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+
+            // Ktor
+            implementation(libs.ktor.client.okhttp)
+        }
+        commonMain.dependencies {
+            implementation(compose.preview)
+
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+//            implementation(compose.material3)
+            implementation(libs.androidx.material3)
+            implementation(compose.material3AdaptiveNavigationSuite)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+
+            api(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+
+            implementation(libs.kotlin.result)
+
+            implementation(libs.kotlinx.serialization.json)
+
+            implementation(libs.navigation.compose)
+
+            implementation(libs.kotlinx.datetime)
+
+            // Shimmer
+            implementation(libs.compose.shimmer)
+
+            // Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.okhttp)
+
+            // Datastore
+            implementation(libs.androidx.datastore.preferences)
+
+            // Room
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.androidx.room.paging)
+
+            // Ktor
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.client.serialization.kotlinx.json)
+
+
+            // TODO Paging
+            implementation(libs.androidx.paging.runtime)
+            implementation(libs.androidx.paging.compose)
+        }
+    }
 }
 
 android {
@@ -19,6 +104,17 @@ android {
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // -- OPEN FOOD FACTS --
+        // https://openfoodfacts.github.io/openfoodfacts-server/api/#authentication
+        // Sorry no email 😭
+        // https://pub.dev/packages/openfoodfacts#migrating-from-2xx-to-3xx-breaking-changes
+        // https://pub.dev/packages/openfoodfacts#setup-optional
+        buildConfigField(
+            "String",
+            "OPEN_FOOD_FACTS_USER_AGENT",
+            "\"FoodYou/$versionName-opensource (https://github.com/maksimowiczm/FoodYou)\""
+        )
     }
 
     android {
@@ -35,90 +131,37 @@ android {
                 "proguard-rules.pro"
             )
 
+            buildConfigField(
+                "String",
+                "OPEN_FOOD_FACTS_URL",
+                "\"https://world.openfoodfacts.org/\""
+            )
+
             // Test minified version with debug signing config
             // signingConfig = signingConfigs.getByName("debug")
+        }
+
+        debug {
+            buildConfigField(
+                "String",
+                "OPEN_FOOD_FACTS_URL",
+                "\"https://world.openfoodfacts.net/\""
+            )
+
+            // Use cached open food facts data for development. See
+            // dev/open-food-facts-cache directory for more information
+            // buildConfigField("String", "OPEN_FOOD_FACTS_URL", "\"<cache-address>\"")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-        freeCompilerArgs += "-Xwhen-guards"
-    }
 
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    flavorDimensions += "version"
-
-    productFlavors {
-        /**
-         * Open source flavour. This flavour is used for the open source version of the app. It
-         * allows only the open source dependencies to be used.
-         */
-        create("opensource") {
-            dimension = "version"
-            isDefault = true
-
-            buildFeatures {
-                viewBinding = true
-            }
-
-            defaultConfig {
-                // -- OPEN FOOD FACTS --
-                // https://openfoodfacts.github.io/openfoodfacts-server/api/#authentication
-                // Sorry no email 😭
-                // https://pub.dev/packages/openfoodfacts#migrating-from-2xx-to-3xx-breaking-changes
-                // https://pub.dev/packages/openfoodfacts#setup-optional
-                buildConfigField(
-                    "String",
-                    "OPEN_FOOD_FACTS_USER_AGENT",
-                    "\"FoodYou/$versionName-opensource (https://github.com/maksimowiczm/FoodYou)\""
-                )
-            }
-
-            buildTypes {
-                debug {
-                    buildConfigField(
-                        "String",
-                        "OPEN_FOOD_FACTS_URL",
-                        "\"https://world.openfoodfacts.net/\""
-                    )
-
-                    // Use cached open food facts data for development. See
-                    // dev/open-food-facts-cache directory for more information
-                    // buildConfigField("String", "OPEN_FOOD_FACTS_URL", "\"<cache-address>\"")
-                }
-
-                release {
-                    buildConfigField(
-                        "String",
-                        "OPEN_FOOD_FACTS_URL",
-                        "\"https://world.openfoodfacts.org/\""
-                    )
-                }
-            }
-        }
-
-        /**
-         * Use this flavour to verify that core can be compiled.
-         */
-        create("dummy") {
-            dimension = "version"
-
-            versionNameSuffix = "-dummy"
-        }
-    }
-
-    sourceSets {
-        getByName("opensource") {
-            res.srcDirs("src/opensource/res")
-            java.srcDirs("src/opensource/java")
-        }
+        viewBinding = true
     }
 }
 
@@ -127,67 +170,8 @@ room {
 }
 
 dependencies {
-
-    // Paging
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.compose)
-
-    // Shimmer
-    implementation(libs.compose.shimmer)
-
-    // Coil
-    "opensourceImplementation"(libs.coil.compose)
-    "opensourceImplementation"(libs.coil.network.okhttp)
-
-    implementation(libs.accompanist.permissions)
-
-    // Zxing
-    "opensourceImplementation"(libs.zxing.android.embedded)
-
-    // Utils
-    implementation(libs.kotlin.result)
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.kotlinx.datetime)
-
-    // Ktor
-    "opensourceImplementation"(libs.ktor.client.core)
-    "opensourceImplementation"(libs.ktor.client.okhttp)
-    "opensourceImplementation"(libs.ktor.client.content.negotiation)
-    "opensourceImplementation"(libs.ktor.client.serialization.kotlinx.json)
-
-    // Datastore
-    implementation(libs.androidx.datastore.preferences)
-
-    // Room
-    "opensourceImplementation"(libs.androidx.room.runtime)
-    ksp(libs.androidx.room.compiler)
-    "opensourceImplementation"(libs.androidx.room.ktx)
-    "opensourceImplementation"(libs.androidx.room.paging)
-
-    // Koin
-    implementation(libs.koin.android)
-    implementation(libs.koin.androidx.compose)
-    implementation(libs.koin.androidx.compose.navigation)
-
-    // Androidx compose
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.foundation)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.icons.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-
-    androidTestImplementation(libs.androidx.test.core.ktx)
-    androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(libs.junit)
+    debugImplementation(compose.uiTooling)
+    listOf("kspAndroid").forEach {
+        add(it, libs.androidx.room.compiler)
+    }
 }
