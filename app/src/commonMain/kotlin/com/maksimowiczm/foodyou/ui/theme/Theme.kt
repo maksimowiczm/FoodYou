6762302af2f52f8f65a9b8aebfbcb402d5124ef2
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-14 17:55:14 +0100

    move Android specific theme code to Android source set

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/ui/theme/Theme.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/ui/theme/Theme.kt
@@ -1,54 +1,6 @@
 package com.maksimowiczm.foodyou.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-)
 
 @Composable
-fun FoodYouTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    val nutrientsPalette = if (darkTheme) DarkNutrientsPalette else LightNutrientsPalette
-
-    CompositionLocalProvider(
-        LocalNutrientsPalette provides nutrientsPalette
-    ) {
-        MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
-        )
-    }
-}
+expect fun FoodYouTheme(content: @Composable () -> Unit)
