486894d4a39f81f2e483cecac72f3a5fd2d0cae5
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-14 17:40:52 +0100

    add paging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,9 +84,7 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.serialization.kotlinx.json)
 
-            // TODO Paging
             implementation(libs.androidx.paging.runtime)
-            implementation(libs.androidx.paging.compose)
         }
     }
 }
