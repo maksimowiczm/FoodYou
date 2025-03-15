5d062121d8243711c70329fe7a763801e804c9d4
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-15 21:28:46 +0100

    simplify android application class

--- app/src/androidMain/kotlin/com/maksimowiczm/foodyou/infrastructure/android/FoodYouApplication.kt
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/infrastructure/android/FoodYouApplication.kt
@@ -3,6 +3,16 @@ package com.maksimowiczm.foodyou.infrastructure.android
 import android.app.Application
 import com.maksimowiczm.foodyou.feature.Feature
 import com.maksimowiczm.foodyou.feature.FeatureManager
+import com.maksimowiczm.foodyou.feature.home.calendarcard.CalendarCard
+import com.maksimowiczm.foodyou.feature.home.caloriescard.CaloriesCard
+import com.maksimowiczm.foodyou.feature.home.mealscard.MealsCard
+import com.maksimowiczm.foodyou.feature.settings.aboutsettings.AboutSettings
+import com.maksimowiczm.foodyou.feature.settings.goalssettings.GoalsSettings
+import com.maksimowiczm.foodyou.feature.settings.language.LanguageSettings
+import com.maksimowiczm.foodyou.feature.settings.language.ui.AndroidTrailingContent
+import com.maksimowiczm.foodyou.feature.settings.mealssettings.MealsSettings
+import com.maksimowiczm.foodyou.feature.settings.openfoodfactssettings.OpenFoodFactsSettings
+import com.maksimowiczm.foodyou.feature.settings.security.SecureScreenSettings
 import com.maksimowiczm.foodyou.infrastructure.di.initKoin
 import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.koin.androidContext
@@ -11,12 +21,33 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-abstract class FoodYouApplication :
+class FoodYouApplication :
     Application(),
     KoinComponent {
-    protected abstract fun FeatureManager.setupFeatures()
+    fun FeatureManager.setupFeatures() {
+        addHomeFeature(
+            CalendarCard,
+            MealsCard(
+                searchHintBuilder = OpenFoodFactsSettings
+            ),
+            CaloriesCard
+        )
 
-    final override fun onCreate() {
+        addSettingsFeature(
+            OpenFoodFactsSettings,
+            MealsSettings,
+            GoalsSettings,
+            SecureScreenSettings,
+            LanguageSettings(
+                languageSettingsTrailingContent = { modifier ->
+                    AndroidTrailingContent(modifier)
+                }
+            ),
+            AboutSettings
+        )
+    }
+
+    override fun onCreate() {
         super.onCreate()
 
         val featureManager = FeatureManager()
