5ceb52883fbc59f883adede4b1d2b4565abc45ae
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-17 22:17:49 +0100

    sort meals in MealsSettingsScreenViewModel

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/settings/mealssettings/ui/MealsSettingsScreenViewModel.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/settings/mealssettings/ui/MealsSettingsScreenViewModel.kt
@@ -5,13 +5,19 @@ import androidx.lifecycle.viewModelScope
 import com.maksimowiczm.foodyou.data.DiaryRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.runBlocking
 
 class MealsSettingsScreenViewModel(diaryRepository: DiaryRepository) : ViewModel() {
-    val meals = diaryRepository.observeMeals().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(2_000),
-        initialValue = runBlocking { diaryRepository.observeMeals().first() }
-    )
+    private fun DiaryRepository.observeSortedMeals() =
+        observeMeals().map { it.sortedBy { it.lexoRank } }
+
+    val meals = diaryRepository.observeSortedMeals()
+        .map { it.sortedBy { it.lexoRank } }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2_000),
+            initialValue = runBlocking { diaryRepository.observeSortedMeals().first() }
+        )
 }
