21cd1bcafdb5176076011aff37720017490b1803
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-17 19:58:09 +0100

    replace LazyColumn with Column

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/settings/mealssettings/ui/MealsSettingsScreen.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/settings/mealssettings/ui/MealsSettingsScreen.kt
@@ -1,27 +1,36 @@
 package com.maksimowiczm.foodyou.feature.settings.mealssettings.ui
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.exclude
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.maksimowiczm.foodyou.data.model.Meal
@@ -76,46 +85,49 @@ fun MealsSettingsScreen(onBack: () -> Unit, meals: List<Meal>, modifier: Modifie
                 scrollBehavior = topBarScrollBehavior
             )
         },
-        modifier = modifier.imePadding()
+        modifier = modifier
+            .imePadding()
+            .nestedScroll(topBarScrollBehavior.nestedScrollConnection),
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets
+            .exclude(ScaffoldDefaults.contentWindowInsets.only(WindowInsetsSides.Bottom))
     ) { paddingValues ->
-        LazyColumn(
-            contentPadding = paddingValues
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(paddingValues)
+                .animateContentSize()
         ) {
-            items(
-                items = meals,
-                key = { meal -> meal.id }
-            ) { meal ->
-                MealSettingsCard(
-                    viewModel = MealSettingsCardViewModel(
-                        diaryRepository = koinInject(),
-                        stringFormatRepository = koinInject(),
-                        mealId = meal.id,
-                        coroutineScope = coroutineScope
-                    ),
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .animateItem()
-                )
+            meals.forEachIndexed { i, meal ->
+                key(meal.id) {
+                    MealSettingsCard(
+                        viewModel = MealSettingsCardViewModel(
+                            diaryRepository = koinInject(),
+                            stringFormatRepository = koinInject(),
+                            mealId = meal.id,
+                            coroutineScope = coroutineScope
+                        ),
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
 
-                Spacer(Modifier.height(8.dp))
+                    Spacer(Modifier.height(8.dp))
+                }
             }
 
-            item(
-                key = "create"
-            ) {
+            key("create") {
                 CreateMealSettingsCard(
                     isCreating = isCreating,
                     onCreatingChange = { isCreating = it },
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .animateItem(),
+                    modifier = Modifier.padding(horizontal = 16.dp),
                     coroutineScope = coroutineScope
                 )
             }
 
-            item {
-                Spacer(Modifier.height(8.dp))
-            }
+            Spacer(Modifier.height(8.dp))
+
+            val bottomPadding = ScaffoldDefaults.contentWindowInsets
+                .only(WindowInsetsSides.Bottom)
+                .asPaddingValues().calculateBottomPadding()
+            Spacer(Modifier.height(bottomPadding))
         }
     }
 }
