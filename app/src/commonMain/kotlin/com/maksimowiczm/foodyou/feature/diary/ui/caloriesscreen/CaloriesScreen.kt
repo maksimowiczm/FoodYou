889dc5482915af45b364e437ed05e30006c5eb3d
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-23 16:33:18 +0100

    improve incomplete product data display

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/ui/caloriesscreen/CaloriesScreen.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/ui/caloriesscreen/CaloriesScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -400,19 +401,6 @@ private fun CaloriesScreen(
                 }
 
                 item {
-                    Text(
-                        text =
-                        "$PREFIX_INCOMPLETE_NUTRIENT_DATA " +
-                            stringResource(
-                                Res.string.description_incomplete_nutrition_data
-                            ),
-                        style = MaterialTheme.typography.labelMedium,
-                        color = MaterialTheme.colorScheme.outline,
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-                    )
-                }
-
-                item {
                     val anyProductIncomplete = diaryDay.meals
                         .filter { it.id in enabledMeals }
                         .flatMap { diaryDay.mealProductMap[it] ?: emptyList() }
@@ -427,7 +415,19 @@ private fun CaloriesScreen(
                             )
                         ) {
                             Text(
-                                text = "Incomplete products",
+                                text =
+                                "$PREFIX_INCOMPLETE_NUTRIENT_DATA " +
+                                    stringResource(
+                                        Res.string.description_incomplete_nutrition_data
+                                    ),
+                                style = MaterialTheme.typography.labelLarge,
+                                color = MaterialTheme.colorScheme.outline
+                            )
+
+                            Spacer(Modifier.height(8.dp))
+
+                            Text(
+                                text = stringResource(Res.string.headline_incomplete_products),
                                 style = MaterialTheme.typography.titleMedium,
                                 color = MaterialTheme.colorScheme.outline
                             )
