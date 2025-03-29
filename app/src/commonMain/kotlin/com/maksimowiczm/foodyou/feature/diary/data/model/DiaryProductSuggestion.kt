cfb25aa235a549401c779ed27c1a5c1cbead48ae
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-03-29 12:09:05 +0100

    remove ProductWithMeasurement and DiaryProductSuggestion

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/data/model/DiaryProductSuggestion.kt
+++
@@ -1,6 +0,0 @@
-package com.maksimowiczm.foodyou.feature.diary.data.model
-
-data class DiaryProductSuggestion(
-    override val product: Product,
-    override val measurement: WeightMeasurement
-) : ProductWithMeasurement
