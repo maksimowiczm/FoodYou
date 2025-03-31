f894a275bed22cab49d2f66b504fe0eb7f9d26e0
Mendel E. <mendel@elmaleh.it>
2025-03-31 19:25:21 -0400

    Delete unused products when clearing OpenFoodFacts cache

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/data/OpenFoodFactsSettingsRepository.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/data/OpenFoodFactsSettingsRepository.kt
@@ -2,6 +2,7 @@ package com.maksimowiczm.foodyou.feature.diary.data
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.maksimowiczm.foodyou.feature.diary.data.model.ProductSource
 import com.maksimowiczm.foodyou.feature.diary.data.preferences.OpenFoodFactsPreferences
 import com.maksimowiczm.foodyou.feature.diary.database.dao.OpenFoodFactsDao
 import com.maksimowiczm.foodyou.feature.system.data.SystemInfoRepository
@@ -67,5 +68,6 @@ class OpenFoodFactsSettingsRepository(
 
     suspend fun clearCache() {
         openFoodFactsDao.clearPagingKeys()
+        openFoodFactsDao.deleteUnusedProducts(ProductSource.OpenFoodFacts)
     }
 }
--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/database/dao/OpenFoodFactsDao.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/diary/database/dao/OpenFoodFactsDao.kt
@@ -3,6 +3,7 @@ package com.maksimowiczm.foodyou.feature.diary.database.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
+import com.maksimowiczm.foodyou.feature.diary.data.model.ProductSource
 import com.maksimowiczm.foodyou.feature.diary.database.entity.OpenFoodFactsPagingKey
 
 @Dao
@@ -26,4 +27,15 @@ interface OpenFoodFactsDao {
         """
     )
     suspend fun clearPagingKeys()
+
+    @Query(
+        """
+        DELETE FROM ProductEntity WHERE id IN (
+            SELECT product.id FROM ProductEntity AS product
+            LEFT JOIN WeightMeasurementEntity AS meal ON meal.productId = product.id 
+            WHERE meal.productId IS NULL AND product.productSource = :source
+        )
+        """
+    )
+    suspend fun deleteUnusedProducts(source: ProductSource)
 }
