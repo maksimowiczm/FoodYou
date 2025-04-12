6548698fc05088a9e8a934dcb012af0df425d86b
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-04-12 12:28:11 +0200

    refactor: use `titleMedium` text style in nutrients list

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/ui/component/NutrientsList.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/ui/component/NutrientsList.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.maksimowiczm.foodyou.core.model.NutrientValue
@@ -41,184 +43,188 @@ fun NutrientsList(
 ) {
     val nutrientsPalette = LocalNutrientsPalette.current
 
-    Column(
-        modifier = modifier
+    CompositionLocalProvider(
+        LocalTextStyle provides MaterialTheme.typography.titleMedium
     ) {
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.unit_calories)
-                )
-            },
-            value = {
-                val value = nutrients.calories.value.formatClipZeros()
-                val kcal = stringResource(Res.string.unit_kcal)
-                Text(text = "$value $kcal")
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+        Column(
+            modifier = modifier
+        ) {
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.unit_calories)
+                    )
+                },
+                value = {
+                    val value = nutrients.calories.value.formatClipZeros()
+                    val kcal = stringResource(Res.string.unit_kcal)
+                    Text(text = "$value $kcal")
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_proteins),
-                    color = nutrientsPalette.proteinsOnSurfaceContainer
-                )
-            },
-            value = {
-                val value = nutrients.proteins.value.formatClipZeros()
-                val g = stringResource(Res.string.unit_gram_short)
-                Text(text = "$value $g")
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_proteins),
+                        color = nutrientsPalette.proteinsOnSurfaceContainer
+                    )
+                },
+                value = {
+                    val value = nutrients.proteins.value.formatClipZeros()
+                    val g = stringResource(Res.string.unit_gram_short)
+                    Text(text = "$value $g")
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_carbohydrates),
-                    color = nutrientsPalette.carbohydratesOnSurfaceContainer
-                )
-            },
-            value = {
-                val value = nutrients.carbohydrates.value.formatClipZeros()
-                val g = stringResource(Res.string.unit_gram_short)
-                Text(text = "$value $g")
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_carbohydrates),
+                        color = nutrientsPalette.carbohydratesOnSurfaceContainer
+                    )
+                },
+                value = {
+                    val value = nutrients.carbohydrates.value.formatClipZeros()
+                    val g = stringResource(Res.string.unit_gram_short)
+                    Text(text = "$value $g")
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_sugars)
-                )
-            },
-            value = {
-                val summary = nutrients.sugars
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_sugars)
+                    )
+                },
+                value = {
+                    val summary = nutrients.sugars
 
-                when (summary) {
-                    is NutrientValue.Incomplete -> incompleteValue(summary)()
-                    is NutrientValue.Complete -> {
-                        val g = stringResource(Res.string.unit_gram_short)
-                        val value = summary.value.formatClipZeros()
-                        Text("$value $g")
+                    when (summary) {
+                        is NutrientValue.Incomplete -> incompleteValue(summary)()
+                        is NutrientValue.Complete -> {
+                            val g = stringResource(Res.string.unit_gram_short)
+                            val value = summary.value.formatClipZeros()
+                            Text("$value $g")
+                        }
                     }
-                }
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_fats),
-                    color = nutrientsPalette.fatsOnSurfaceContainer
-                )
-            },
-            value = {
-                val value = nutrients.fats.value.formatClipZeros()
-                val g = stringResource(Res.string.unit_gram_short)
-                Text(text = "$value $g")
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_fats),
+                        color = nutrientsPalette.fatsOnSurfaceContainer
+                    )
+                },
+                value = {
+                    val value = nutrients.fats.value.formatClipZeros()
+                    val g = stringResource(Res.string.unit_gram_short)
+                    Text(text = "$value $g")
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_saturated_fats)
-                )
-            },
-            value = {
-                val summary = nutrients.saturatedFats
-                when (summary) {
-                    is NutrientValue.Incomplete -> incompleteValue(summary)()
-                    is NutrientValue.Complete -> {
-                        val g = stringResource(Res.string.unit_gram_short)
-                        val value = summary.value.formatClipZeros()
-                        Text("$value $g")
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_saturated_fats)
+                    )
+                },
+                value = {
+                    val summary = nutrients.saturatedFats
+                    when (summary) {
+                        is NutrientValue.Incomplete -> incompleteValue(summary)()
+                        is NutrientValue.Complete -> {
+                            val g = stringResource(Res.string.unit_gram_short)
+                            val value = summary.value.formatClipZeros()
+                            Text("$value $g")
+                        }
                     }
-                }
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_salt)
-                )
-            },
-            value = {
-                val summary = nutrients.salt
-                when (summary) {
-                    is NutrientValue.Incomplete -> incompleteValue(summary)()
-                    is NutrientValue.Complete -> {
-                        val g = stringResource(Res.string.unit_gram_short)
-                        val value = summary.value.formatClipZeros()
-                        Text("$value $g")
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_salt)
+                    )
+                },
+                value = {
+                    val summary = nutrients.salt
+                    when (summary) {
+                        is NutrientValue.Incomplete -> incompleteValue(summary)()
+                        is NutrientValue.Complete -> {
+                            val g = stringResource(Res.string.unit_gram_short)
+                            val value = summary.value.formatClipZeros()
+                            Text("$value $g")
+                        }
                     }
-                }
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_fiber)
-                )
-            },
-            value = {
-                val summary = nutrients.fiber
-                when (summary) {
-                    is NutrientValue.Incomplete -> incompleteValue(summary)()
-                    is NutrientValue.Complete -> {
-                        val g = stringResource(Res.string.unit_gram_short)
-                        val value = summary.value.formatClipZeros()
-                        Text("$value $g")
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_fiber)
+                    )
+                },
+                value = {
+                    val summary = nutrients.fiber
+                    when (summary) {
+                        is NutrientValue.Incomplete -> incompleteValue(summary)()
+                        is NutrientValue.Complete -> {
+                            val g = stringResource(Res.string.unit_gram_short)
+                            val value = summary.value.formatClipZeros()
+                            Text("$value $g")
+                        }
                     }
-                }
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
 
-        HorizontalDivider(Modifier.padding(horizontal = 48.dp))
+            HorizontalDivider(Modifier.padding(horizontal = 48.dp))
 
-        NutrientListItem(
-            label = {
-                Text(
-                    text = stringResource(Res.string.nutriment_sodium)
-                )
-            },
-            value = {
-                val summary = nutrients.sodium
-                when (summary) {
-                    is NutrientValue.Incomplete -> incompleteValue(summary)()
-                    is NutrientValue.Complete -> {
-                        val g = stringResource(Res.string.unit_gram_short)
-                        val value = summary.value.formatClipZeros()
-                        Text("$value $g")
+            NutrientListItem(
+                label = {
+                    Text(
+                        text = stringResource(Res.string.nutriment_sodium)
+                    )
+                },
+                value = {
+                    val summary = nutrients.sodium
+                    when (summary) {
+                        is NutrientValue.Incomplete -> incompleteValue(summary)()
+                        is NutrientValue.Complete -> {
+                            val g = stringResource(Res.string.unit_gram_short)
+                            val value = summary.value.formatClipZeros()
+                            Text("$value $g")
+                        }
                     }
-                }
-            },
-            modifier = Modifier.padding(vertical = 8.dp)
-        )
+                },
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
     }
 }
 
