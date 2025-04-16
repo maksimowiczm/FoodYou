7dccb6792388b65a07b91cf4aeb848dbab3f81d0
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-04-17 00:59:09 +0200

    improve download UI

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/productredesign/DownloadOpenFoodFactsProduct.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/productredesign/DownloadOpenFoodFactsProduct.kt
@@ -1,6 +1,10 @@
 package com.maksimowiczm.foodyou.feature.productredesign
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,14 +12,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Link
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -26,8 +38,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.animateFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -35,9 +49,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.maksimowiczm.foodyou.core.ui.ext.plus
 import foodyou.app.generated.resources.*
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -46,17 +62,24 @@ internal fun DownloadOpenFoodFactsProduct(
     animatedVisibilityScope: AnimatedVisibilityScope,
     contentPadding: PaddingValues,
     onSearch: () -> Unit,
-    onDownload: () -> Unit,
+    onDownload: (url: String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val layoutDirection = LocalLayoutDirection.current
-    val insets = remember(contentPadding, layoutDirection) {
-        WindowInsets(
-            left = contentPadding.calculateLeftPadding(layoutDirection),
-            right = contentPadding.calculateRightPadding(layoutDirection),
-            top = contentPadding.calculateTopPadding(),
-            bottom = contentPadding.calculateBottomPadding()
-        )
+    val insets = WindowInsets(
+        left = contentPadding.calculateLeftPadding(layoutDirection),
+        right = contentPadding.calculateRightPadding(layoutDirection),
+        top = contentPadding.calculateTopPadding(),
+        bottom = contentPadding.calculateBottomPadding()
+    )
+
+    val linkTextState = rememberTextFieldState()
+    var downloading by remember { mutableStateOf(false) }
+    LaunchedEffect(downloading) {
+        if (downloading) {
+            delay(3_000)
+            downloading = false
+        }
     }
 
     var fabHeight by remember { mutableIntStateOf(0) }
@@ -81,41 +104,85 @@ internal fun DownloadOpenFoodFactsProduct(
                 ) {
                     Icon(
                         imageVector = Icons.Default.Search,
-                        contentDescription = null
+                        contentDescription = stringResource(Res.string.action_search)
                     )
                 }
                 ExtendedFloatingActionButton(
-                    onClick = onDownload
+                    onClick = {
+                        downloading = true
+                    }
                 ) {
                     Icon(
                         imageVector = Icons.Default.Download,
-                        contentDescription = null
+                        contentDescription = stringResource(Res.string.action_download)
                     )
                     Spacer(Modifier.width(8.dp))
-                    Text("Download")
+                    Text(stringResource(Res.string.action_download))
                 }
             }
         },
         contentWindowInsets = insets
     ) { paddingValues ->
         LazyColumn(
-            contentPadding = paddingValues + PaddingValues(horizontal = 16.dp),
+            contentPadding = paddingValues,
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
+            stickyHeader {
+                AnimatedContent(
+                    targetState = downloading,
+                    modifier = Modifier.fillMaxWidth(),
+                    transitionSpec = { fadeIn() togetherWith fadeOut() }
+                ) {
+                    if (it) {
+                        LinearProgressIndicator()
+                    } else {
+                        Spacer(Modifier.height(4.dp))
+                    }
+                }
+            }
+
             item {
                 Text(
                     text = stringResource(Res.string.open_food_facts_disclaimer),
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
 
             item {
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = {},
-                    label = { Text("Product link") }
+                    state = linkTextState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Link,
+                            contentDescription = null
+                        )
+                    },
+                    trailingIcon = {
+                        if (linkTextState.text.isNotEmpty()) {
+                            IconButton(
+                                onClick = linkTextState::clearText
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Clear,
+                                    contentDescription = stringResource(Res.string.action_clear)
+                                )
+                            }
+                        }
+                    },
+                    label = { Text(stringResource(Res.string.product_link)) },
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
+                    onKeyboardAction = {
+                        if (linkTextState.text.isNotEmpty()) {
+                            downloading = true
+                        }
+                    }
                 )
             }
 
@@ -125,11 +192,11 @@ internal fun DownloadOpenFoodFactsProduct(
                     LocalTextStyle provides MaterialTheme.typography.bodySmall
                 ) {
                     Column(
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
                     ) {
-                        Text("Example links")
-                        Text("https://world.openfoodfacts.org/product/1234567890123")
-                        Text("https://world.openfoodfacts.org/product/1234567890123/product-name")
+                        Text(stringResource(Res.string.headline_example_links))
+                        Text(stringResource(Res.string.link_open_food_facts_product_example_1))
+                        Text(stringResource(Res.string.link_open_food_facts_product_example_2))
                     }
                 }
             }
