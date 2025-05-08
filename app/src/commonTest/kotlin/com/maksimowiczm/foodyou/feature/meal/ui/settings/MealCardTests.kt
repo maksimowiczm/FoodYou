db8612a5a5be44f26df9fda2d650b7b2176c9e3e
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-08 11:37:16 +0200

    test(meal): fix `MealCardTests`

--- app/src/commonTest/kotlin/com/maksimowiczm/foodyou/feature/meal/ui/settings/MealCardTests.kt
+++ app/src/commonTest/kotlin/com/maksimowiczm/foodyou/feature/meal/ui/settings/MealCardTests.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.unit.dp
-import com.maksimowiczm.foodyou.feature.meal.domain.Meal
+import com.maksimowiczm.foodyou.core.domain.model.Meal
 import com.maksimowiczm.foodyou.feature.meal.ui.settings.MealCardTestTags.ALL_DAY_SWITCH
 import com.maksimowiczm.foodyou.feature.meal.ui.settings.MealCardTestTags.CONFIRM_BUTTON
 import com.maksimowiczm.foodyou.feature.meal.ui.settings.MealCardTestTags.DELETE_BUTTON
@@ -41,7 +41,8 @@ class MealCardTests {
             id = 0,
             name = name,
             from = from,
-            to = to
+            to = to,
+            rank = 0
         )
 
         setContent {
