8b846dc66a45b59e0487f2f2464638530666c7e9
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-09 16:51:02 +0200

    move it up because git is confused

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/meal/ui/card/MealsCard.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/meal/ui/card/MealsCard.kt
@@ -102,6 +102,56 @@ internal fun MealsCard(
 
 @OptIn(ExperimentalSharedTransitionApi::class, ExperimentalAnimationApi::class)
 @Composable
+private fun VerticalMealsCard(
+    meals: List<MealWithSummary>?,
+    onMealClick: (mealId: Long) -> Unit,
+    onAddClick: (mealId: Long) -> Unit,
+    onLongClick: () -> Unit,
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    epochDay: Int,
+    contentPadding: PaddingValues,
+    shimmer: Shimmer,
+    modifier: Modifier = Modifier
+) = with(LocalNavigationSharedTransitionScope.current ?: error("SharedTransitionScope not found")) {
+    val transition = updateTransition(meals)
+
+    val meals = remember(meals) {
+        meals ?: List<MealWithSummary?>(4) { null }
+    }
+
+    Column(
+        modifier = modifier.padding(contentPadding),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        meals.forEachIndexed { i, meal ->
+            transition.Crossfade(
+                contentKey = { it != null },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                if (it != null && meal != null) {
+                    MealCard(
+                        animatedVisibilityScope = animatedVisibilityScope,
+                        epochDay = epochDay,
+                        meal = meal,
+                        isEmpty = meal.isEmpty,
+                        totalCalories = meal.calories,
+                        totalProteins = meal.proteins,
+                        totalCarbohydrates = meal.carbohydrates,
+                        totalFats = meal.fats,
+                        onMealClick = { onMealClick(meal.id) },
+                        onAddClick = { onAddClick(meal.id) },
+                        onLongClick = onLongClick
+                    )
+                } else {
+                    MealCardSkeleton(shimmer)
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalAnimationApi::class)
+@Composable
 private fun HorizontalMealsCard(
     meals: List<MealWithSummary>?,
     onMealClick: (mealId: Long) -> Unit,
@@ -165,56 +215,6 @@ private fun HorizontalMealsCard(
                 MealCardSkeleton(
                     shimmer = shimmer
                 )
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalAnimationApi::class)
-@Composable
-private fun VerticalMealsCard(
-    meals: List<MealWithSummary>?,
-    onMealClick: (mealId: Long) -> Unit,
-    onAddClick: (mealId: Long) -> Unit,
-    onLongClick: () -> Unit,
-    animatedVisibilityScope: AnimatedVisibilityScope,
-    epochDay: Int,
-    contentPadding: PaddingValues,
-    shimmer: Shimmer,
-    modifier: Modifier = Modifier
-) = with(LocalNavigationSharedTransitionScope.current ?: error("SharedTransitionScope not found")) {
-    val transition = updateTransition(meals)
-
-    val meals = remember(meals) {
-        meals ?: List<MealWithSummary?>(4) { null }
-    }
-
-    Column(
-        modifier = modifier.padding(contentPadding),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        meals.forEachIndexed { i, meal ->
-            transition.Crossfade(
-                contentKey = { it != null },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                if (it != null && meal != null) {
-                    MealCard(
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        epochDay = epochDay,
-                        meal = meal,
-                        isEmpty = meal.isEmpty,
-                        totalCalories = meal.calories,
-                        totalProteins = meal.proteins,
-                        totalCarbohydrates = meal.carbohydrates,
-                        totalFats = meal.fats,
-                        onMealClick = { onMealClick(meal.id) },
-                        onAddClick = { onAddClick(meal.id) },
-                        onLongClick = onLongClick
-                    )
-                } else {
-                    MealCardSkeleton(shimmer)
-                }
             }
         }
     }
