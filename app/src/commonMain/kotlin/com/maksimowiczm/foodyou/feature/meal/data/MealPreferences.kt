e0361efb65c6404a0a404720869eac79a6477986
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-09 13:32:32 +0200

    add meal cards layout setting

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/meal/data/MealPreferences.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/meal/data/MealPreferences.kt
@@ -5,4 +5,5 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 internal object MealPreferences {
     val timeBasedSorting = booleanPreferencesKey("time_based_sorting")
     val includeAllDayMeals = booleanPreferencesKey("include_all_day_meals")
+    val useVerticalLayout = booleanPreferencesKey("use_vertical_layout")
 }
