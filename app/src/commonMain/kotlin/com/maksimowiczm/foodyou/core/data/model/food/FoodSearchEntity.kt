0b1612732889a81ae3653c6cfac495644be642bd
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-16 16:09:32 +0200

    docs

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/data/model/food/FoodSearchEntity.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/data/model/food/FoodSearchEntity.kt
@@ -29,8 +29,10 @@ data class FoodSearchEntity(
     // UI id for add food screen. UI id must be unique for each food entity.
     // Entity without measurement id must have same id as first entity with measurement id. Every
     // other entity must have other id.
+    // Yes this is confusing and database shouldn't be responsible for this but it works.
     /**
      * @see [com.maksimowiczm.foodyou.feature.addfood.ui.search.SearchFoodScreen]
+     * @see [com.maksimowiczm.foodyou.core.data.database.food.FoodDao.queryFood]
      */
     val uiId: String
 ) : EntityWithMeasurement
