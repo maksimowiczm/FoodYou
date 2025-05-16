c446204e230ad489ef3111c5c8c769df3a73628a
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-16 10:00:10 +0200

    add BarcodeScannerIconButton component

---
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/ui/component/BarcodeScannerIconButton.kt
@@ -0,0 +1,24 @@
+package com.maksimowiczm.foodyou.core.ui.component
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import foodyou.app.generated.resources.*
+import foodyou.app.generated.resources.Res
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+// Actually works only on android. It uses xml vector drawable.
+@Composable
+fun BarcodeScannerIconButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(Res.drawable.ic_barcode_scanner),
+            contentDescription = stringResource(Res.string.action_scan_barcode)
+        )
+    }
+}
