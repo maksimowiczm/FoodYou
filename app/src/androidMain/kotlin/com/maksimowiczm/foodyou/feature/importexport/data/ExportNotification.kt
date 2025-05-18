14d0d5153d93a56dc8eafd91be83275c26f60008
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-18 14:20:14 +0200

    export progress notification

---
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/feature/importexport/data/ExportNotification.kt
@@ -0,0 +1,17 @@
+package com.maksimowiczm.foodyou.feature.importexport.data
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import foodyou.app.generated.resources.*
+import foodyou.app.generated.resources.Res
+import org.jetbrains.compose.resources.getString
+
+object ExportNotification {
+    const val CHANNEL_ID = "EXPORT_CHANNEL_ID"
+
+    suspend fun getChannel() = NotificationChannel(
+        CHANNEL_ID,
+        getString(Res.string.notification_export_channel_name),
+        NotificationManager.IMPORTANCE_DEFAULT
+    )
+}
--- app/src/androidMain/kotlin/com/maksimowiczm/foodyou/feature/importexport/data/ExportProductsWorker.kt
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/feature/importexport/data/ExportProductsWorker.kt
@@ -1,33 +1,137 @@
 package com.maksimowiczm.foodyou.feature.importexport.data
 
+import android.app.Notification
 import android.content.Context
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.net.toUri
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import co.touchlab.kermit.Logger
+import com.maksimowiczm.foodyou.R
+import com.maksimowiczm.foodyou.core.ext.notifyIfAllowed
+import foodyou.app.generated.resources.*
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.getString
 
 class ExportProductsWorker(context: Context, workerParameters: WorkerParameters) :
     CoroutineWorker(context, workerParameters) {
 
+    private val notificationManager by lazy { NotificationManagerCompat.from(context) }
+
     override suspend fun doWork(): Result {
-        val uriString = inputData.getString("uri") ?: return Result.failure()
+        setForeground(createForegroundInfo(null, null))
+
+        val result = internalDoWork()
+
+        val notification = if (result) {
+            createSuccessNotification()
+        } else {
+            createFailureNotification()
+        }
+
+        notificationManager.notifyIfAllowed(10_001, notification)
+
+        return if (result) {
+            Result.success()
+        } else {
+            Result.failure()
+        }
+    }
+
+    /**
+     * @return true if the work was successful, false otherwise
+     */
+    suspend fun internalDoWork(): Boolean {
+        val uriString = inputData.getString("uri") ?: return false
         val uri = uriString.toUri()
         val resolver = applicationContext.contentResolver
         val inputStream = runCatching {
             resolver.openInputStream(uri)
         }.getOrElse {
             Logger.e(TAG, it) { "Error opening input stream" }
-            return Result.failure()
-        } ?: return Result.failure()
+            return false
+        } ?: return false
 
         return try {
-            TODO()
+            val max = 15
+            for (i in 0..max) {
+                setForeground(createForegroundInfo(max = max, progress = i))
+                delay(1000L)
+            }
+
+            true
         } catch (e: Exception) {
             Logger.e(TAG, e) { "Error exporting products" }
-            Result.failure()
+            false
         } finally {
             inputStream.close()
         }
+    }
+
+    private suspend fun createForegroundInfo(
+        max: Int?,
+        progress: Int?,
+        notificationId: Int = 10_000
+    ): ForegroundInfo {
+        val notification = createProgressNotification(max, progress)
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ForegroundInfo(notificationId, notification, FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else {
+            ForegroundInfo(notificationId, notification)
+        }
+    }
+
+    private suspend fun createProgressNotification(max: Int?, progress: Int?): Notification {
+        val channelId = ExportNotification.CHANNEL_ID
+        val channel = ExportNotification.getChannel()
+        notificationManager.createNotificationChannel(channel)
+
+        return if (max == null || progress == null) {
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(Res.string.notification_exporting_products))
+                .setOngoing(true)
+                .build()
+        } else {
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(Res.string.notification_exporting_products))
+                .setOngoing(true)
+                .setProgress(max, progress, false)
+                .setContentText("$progress / $max")
+                .build()
+        }
+    }
+
+    private suspend fun createSuccessNotification(): Notification {
+        val channelId = ExportNotification.CHANNEL_ID
+        val channel = ExportNotification.getChannel()
+        notificationManager.createNotificationChannel(channel)
+
+        return NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(Res.string.notification_exporting_products_success))
+            .setOngoing(false)
+            .setAutoCancel(true)
+            .build()
+    }
+
+    private suspend fun createFailureNotification(): Notification {
+        val channelId = ExportNotification.CHANNEL_ID
+        val channel = ExportNotification.getChannel()
+        notificationManager.createNotificationChannel(channel)
+
+        return NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(Res.string.notification_exporting_products_failure))
+            .setOngoing(false)
+            .setAutoCancel(true)
+            .build()
     }
 
     private companion object {
