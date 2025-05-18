4e50a7db5b555d3a2dcda51f11b4d5f9ff379397
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-18 19:06:59 +0200

    show toast when import/export starts

--- app/src/androidMain/kotlin/com/maksimowiczm/foodyou/feature/importexport/data/BackupService.kt
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/feature/importexport/data/BackupService.kt
@@ -2,9 +2,14 @@ package com.maksimowiczm.foodyou.feature.importexport.data
 
 import android.content.Context
 import android.net.Uri
+import android.widget.Toast
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
+import foodyou.app.generated.resources.*
+import foodyou.app.generated.resources.Res
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
 
 internal class BackupService(private val context: Context) {
 
@@ -19,6 +24,12 @@ internal class BackupService(private val context: Context) {
             .build()
 
         workManager.enqueue(request)
+
+        Toast.makeText(
+            context,
+            runBlocking { getString(Res.string.neutral_export_started) },
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     fun importFoodProducts(path: Uri) {
@@ -29,5 +40,11 @@ internal class BackupService(private val context: Context) {
             .build()
 
         workManager.enqueue(request)
+
+        Toast.makeText(
+            context,
+            runBlocking { getString(Res.string.neutral_import_started) },
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
