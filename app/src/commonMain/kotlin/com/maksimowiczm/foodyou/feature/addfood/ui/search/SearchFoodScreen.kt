e39c0b20915839bd310419b3f80666147d761953
maksimowiczm <maksimowiczmateusz13@gmail.com>
2025-05-19 12:58:15 +0200

    add loading indicator to search screen

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/addfood/ui/search/SearchFoodScreen.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/addfood/ui/search/SearchFoodScreen.kt
@@ -1,11 +1,16 @@
 package com.maksimowiczm.foodyou.feature.addfood.ui.search
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -30,6 +35,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBarDefaults
@@ -39,6 +45,7 @@ import androidx.compose.material3.ToggleFloatingActionButton
 import androidx.compose.material3.ToggleFloatingActionButtonDefaults
 import androidx.compose.material3.TopSearchBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,6 +53,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -66,6 +74,9 @@ import com.maksimowiczm.foodyou.core.ui.component.Scrim
 import com.maksimowiczm.foodyou.feature.addfood.model.SearchFoodItem
 import com.maksimowiczm.foodyou.feature.barcodescanner.FullScreenCameraBarcodeScanner
 import foodyou.app.generated.resources.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.transform
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -244,6 +255,20 @@ private fun Content(
     val coroutineScope = rememberCoroutineScope()
     var showBarcodeScanner by rememberSaveable { mutableStateOf(false) }
 
+    var showLoadingIndicator by remember { mutableStateOf(true) }
+    LaunchedEffect(pages) {
+        snapshotFlow { pages.loadState }.transform {
+            if (it.isIdle) {
+                emit(it)
+            } else {
+                delay(200)
+                emit(it)
+            }
+        }.collectLatest {
+            showLoadingIndicator = !it.isIdle
+        }
+    }
+
     val inputField = @Composable {
         SearchBarDefaults.InputField(
             textFieldState = state.textFieldState,
@@ -340,6 +365,23 @@ private fun Content(
                     text = stringResource(Res.string.neutral_no_food_found),
                     style = MaterialTheme.typography.bodyLargeEmphasized
                 )
+            }
+        }
+
+        AnimatedVisibility(
+            visible = showLoadingIndicator,
+            modifier = Modifier
+                .zIndex(5f)
+                .fillMaxWidth()
+                .padding(top = paddingValues.calculateTopPadding()),
+            enter = expandVertically(),
+            exit = shrinkVertically() + fadeOut()
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                LoadingIndicator(Modifier)
             }
         }
 
