4829f0d33259bf912657c6d899dd6517c21f5107
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-24 23:16:23 +0200

    drop old measurement stuff

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/addfood/AddFoodModule.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/addfood/AddFoodModule.kt
@@ -1,16 +1,12 @@
 package com.maksimowiczm.foodyou.feature.addfood
 
 import com.maksimowiczm.foodyou.feature.addfood.data.AddFoodRepository
-import com.maksimowiczm.foodyou.feature.addfood.ui.measurement.CreateMeasurementScreenViewModel
-import com.maksimowiczm.foodyou.feature.addfood.ui.measurement.UpdateMeasurementScreenViewModel
 import com.maksimowiczm.foodyou.feature.addfood.ui.search.SearchFoodViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val addFoodModule = module {
     viewModelOf(::SearchFoodViewModel)
-    viewModelOf(::CreateMeasurementScreenViewModel)
-    viewModelOf(::UpdateMeasurementScreenViewModel)
 
     factory {
         AddFoodRepository(
--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/addfood/ui/measurement/MeasurementScreenEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.maksimowiczm.foodyou.feature.addfood.ui.measurement
-
-import com.maksimowiczm.foodyou.core.domain.model.FoodId
-
-internal sealed interface MeasurementScreenEvent {
-    data object Closed : MeasurementScreenEvent
-    data class RecipeClonedIntoProduct(val newId: FoodId.Product) : MeasurementScreenEvent
-    data object FoodDeleted : MeasurementScreenEvent
-}
