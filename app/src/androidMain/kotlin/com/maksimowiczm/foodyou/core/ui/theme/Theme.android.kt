e9f0876a34b611f0527165c0763797eeef8ea249
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-27 22:15:26 +0200

    restore theme

--- app/src/androidMain/kotlin/com/maksimowiczm/foodyou/core/ui/theme/Theme.android.kt
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/core/ui/theme/Theme.android.kt
@@ -3,7 +3,7 @@ package com.maksimowiczm.foodyou.core.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -55,7 +55,7 @@ fun FoodYouTheme(
     CompositionLocalProvider(
         LocalNutrientsPalette provides nutrientsPalette
     ) {
-        MaterialExpressiveTheme(
+        MaterialTheme(
             colorScheme = colorScheme,
             content = content
         )
