522c0b402d48b84e2ad66de6aa5f7d32df4152f4
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-29 08:20:29 +0200

    fix: correct barcode scanner icon content description

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/ui/component/BarcodeScannerIconButton.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/ui/component/BarcodeScannerIconButton.kt
@@ -1,12 +1,10 @@
 package com.maksimowiczm.foodyou.core.ui.component
 
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import foodyou.app.generated.resources.*
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -18,10 +16,7 @@ fun BarcodeScannerIconButton(onClick: () -> Unit, modifier: Modifier = Modifier)
     ) {
         Icon(
             painter = painterResource(Res.drawable.ic_barcode_scanner),
-            contentDescription = stringResource(Res.string.action_go_back)
+            contentDescription = stringResource(Res.string.action_scan_barcode)
         )
     }
 }
-
-val Icons.BarcodeScanner: DrawableResource
-    get() = Res.drawable.ic_barcode_scanner
