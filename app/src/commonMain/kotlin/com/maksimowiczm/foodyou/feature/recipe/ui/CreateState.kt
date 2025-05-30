2d19f118880a6730a47931385fe4295a504c397d
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-30 11:15:27 +0200

    remove old recipe feature

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/recipe/ui/CreateState.kt
+++
@@ -1,9 +0,0 @@
-package com.maksimowiczm.foodyou.feature.recipe.ui
-
-internal sealed interface CreateState {
-    data object Nothing : CreateState
-    data object CreatingRecipe : CreateState
-
-    @JvmInline
-    value class Created(val recipeId: Long) : CreateState
-}
