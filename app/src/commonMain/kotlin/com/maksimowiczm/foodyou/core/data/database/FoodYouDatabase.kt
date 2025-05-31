fb3bc4d83c63309ea2b81cc348f7003415cdef7f
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-31 13:09:23 +0200

    squash database migrations

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/data/database/FoodYouDatabase.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/core/data/database/FoodYouDatabase.kt
@@ -72,8 +72,7 @@ import com.maksimowiczm.foodyou.core.data.model.search.SearchQueryEntity
         AutoMigration(from = 14, to = 15),
         AutoMigration(from = 15, to = 16),
         AutoMigration(from = 16, to = 17),
-        AutoMigration(from = 17, to = 18),
-        AutoMigration(from = 18, to = 19)
+        AutoMigration(from = 17, to = 18)
     ]
 )
 @TypeConverters(
@@ -90,7 +89,7 @@ abstract class FoodYouDatabase : RoomDatabase() {
     abstract val foodDao: FoodDao
 
     companion object {
-        const val VERSION = 19
+        const val VERSION = 18
 
         private val migrations: List<Migration> = listOf(
             MIGRATION_1_2,
