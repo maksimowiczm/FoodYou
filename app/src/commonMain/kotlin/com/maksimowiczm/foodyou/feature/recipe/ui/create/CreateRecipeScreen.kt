3265ecf3a36a114435ce58e20fd9300b25c13b6f
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-05-31 14:21:13 +0200

    add discard dialog

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/recipe/ui/create/CreateRecipeScreen.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/recipe/ui/create/CreateRecipeScreen.kt
@@ -1,13 +1,20 @@
 package com.maksimowiczm.foodyou.feature.recipe.ui.create
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.maksimowiczm.foodyou.core.domain.model.FoodId
+import com.maksimowiczm.foodyou.core.ui.component.BackHandler
 import com.maksimowiczm.foodyou.core.ui.ext.LaunchedCollectWithLifecycle
+import com.maksimowiczm.foodyou.feature.recipe.ui.DiscardDialog
 import com.maksimowiczm.foodyou.feature.recipe.ui.RecipeApp
 import foodyou.app.generated.resources.*
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -21,6 +28,25 @@ internal fun CreateRecipeScreen(
     LaunchedCollectWithLifecycle(viewModel.eventBus) {
         when (it) {
             is CreateRecipeEvent.RecipeCreated -> latestOnCreate(it.id)
+        }
+    }
+
+    var showDiscardDialog by rememberSaveable { mutableStateOf(false) }
+    BackHandler(
+        onBack = { showDiscardDialog = true }
+    )
+
+    if (showDiscardDialog) {
+        DiscardDialog(
+            onDismissRequest = {
+                showDiscardDialog = false
+            },
+            onDiscard = {
+                showDiscardDialog = false
+                onBack()
+            }
+        ) {
+            Text(stringResource(Res.string.question_discard_recipe))
         }
     }
 
