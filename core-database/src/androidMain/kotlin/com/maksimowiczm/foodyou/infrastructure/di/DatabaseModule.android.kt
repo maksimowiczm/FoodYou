8036e01f7e18577f7a1cb2fd529e741f06c33f35
Mateusz Maksimowicz <41268878+maksimowiczm@users.noreply.github.com>
2025-06-10 13:42:45 +0200

    refactor: bundle SQLite (#146)
    Include SQLite in the APK, which with minor modifications enables the app to launch on devices running Android 7.0 (API level 24). 
    
    Bundling SQLite also eliminates the need to worry about which Android SDK supports which SQLite version.

--- core-database/src/androidMain/kotlin/com/maksimowiczm/foodyou/infrastructure/di/DatabaseModule.android.kt
+++ core-database/src/androidMain/kotlin/com/maksimowiczm/foodyou/infrastructure/di/DatabaseModule.android.kt
@@ -1,9 +1,11 @@
 package com.maksimowiczm.foodyou.infrastructure.di
 
+import android.os.Build
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.maksimowiczm.foodyou.core.database.FoodYouDatabase
 import com.maksimowiczm.foodyou.core.database.FoodYouDatabase.Companion.buildDatabase
+import io.requery.android.database.sqlite.RequerySQLiteOpenHelperFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.definition.KoinDefinition
 import org.koin.core.module.Module
@@ -16,6 +18,12 @@ actual val databaseDefinition: Module.() -> KoinDefinition<FoodYouDatabase> = {
                 klass = FoodYouDatabase::class.java,
                 name = DATABASE_NAME
             )
+
+        // https://developer.android.com/reference/android/database/sqlite/package-summary
+        // Require SQLite version >= 3.28
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            builder.openHelperFactory(RequerySQLiteOpenHelperFactory())
+        }
 
         builder.buildDatabase()
     }
