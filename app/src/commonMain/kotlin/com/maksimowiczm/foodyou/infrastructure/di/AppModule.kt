42de2af25db524396539fffbafd349f1fd93dc00
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-06-12 10:56:38 +0200

    clean up old about page

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/infrastructure/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package com.maksimowiczm.foodyou.infrastructure.di
-
-import com.maksimowiczm.foodyou.ui.about.AboutSettingsViewModel
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
-
-val appModule = module {
-    viewModelOf(::AboutSettingsViewModel)
-}
