9bdc005649c0941499ed95bed3a7484564fbe700
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-07-05 14:38:09 +0200

    add MeasurementPicker component to ProductForm

--- feature3/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/food/ui/product/ProductForm.kt
+++ feature3/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/food/ui/product/ProductForm.kt
@@ -1,24 +1,43 @@
 package com.maksimowiczm.foodyou.feature.food.ui.product
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SplitButtonDefaults
+import androidx.compose.material3.SplitButtonLayout
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.maksimowiczm.foodyou.core.ui.form.FormField
+import com.maksimowiczm.foodyou.feature.measurement.domain.Measurement
+import com.maksimowiczm.foodyou.feature.measurement.ui.stringResource
 import foodyou.app.generated.resources.*
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -78,6 +97,15 @@ internal fun ProductForm(
             modifier = Modifier.padding(horizontalPadding).fillMaxWidth(),
             imeAction = ImeAction.Next,
             supportingText = stringResource(Res.string.description_add_note)
+        )
+
+        MeasurementPicker(
+            selected = state.measurement,
+            onSelect = { state.measurement = it },
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .padding(horizontalPadding)
+                .fillMaxWidth()
         )
     }
 }
@@ -170,4 +198,81 @@ private fun BarcodeTextField(
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+private fun MeasurementPicker(
+    selected: Measurement,
+    onSelect: (Measurement) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val possibleValues = remember {
+        listOf(
+            Measurement.Gram(100f),
+            Measurement.Milliliter(100f),
+            Measurement.Serving(1f),
+            Measurement.Package(1f)
+        )
+    }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(Res.string.headline_values_per),
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        SplitButtonLayout(
+            leadingButton = {
+                SplitButtonDefaults.LeadingButton(
+                    onClick = { expanded = !expanded }
+                ) {
+                    Text(
+                        text = selected.stringResource()
+                    )
+                }
+            },
+            trailingButton = {
+                SplitButtonDefaults.TrailingButton(
+                    checked = expanded,
+                    onCheckedChange = { expanded = it }
+                ) {
+                    val rotation by animateFloatAsState(
+                        targetValue = if (expanded) 180f else 0f
+                    )
+
+                    Icon(
+                        Icons.Filled.KeyboardArrowDown,
+                        modifier =
+                        Modifier.size(SplitButtonDefaults.TrailingIconSize).graphicsLayer {
+                            this.rotationZ = rotation
+                        },
+                        contentDescription = null
+                    )
+                }
+
+                // Had to put it here because it must be aligned to the trailing button
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    possibleValues.forEach {
+                        DropdownMenuItem(
+                            text = { Text(it.stringResource()) },
+                            onClick = {
+                                expanded = false
+                                onSelect(it)
+                            }
+                        )
+                    }
+                }
+            }
+        )
+    }
 }
