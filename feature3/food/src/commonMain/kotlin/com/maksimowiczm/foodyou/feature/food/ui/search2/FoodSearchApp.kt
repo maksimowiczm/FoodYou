67ffe0aea124eb7f6a25a619f759cd7cedf229ac
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-07-26 11:02:01 +0200

    display pages

--- feature3/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/food/ui/search2/FoodSearchApp.kt
+++ feature3/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/food/ui/search2/FoodSearchApp.kt
@@ -11,23 +11,31 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.FilterAlt
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ContainedLoadingIndicator
 import androidx.compose.material3.ExpandedFullScreenSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -43,23 +51,34 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
 import com.maksimowiczm.foodyou.core.preferences.collectAsStateWithLifecycle
 import com.maksimowiczm.foodyou.core.preferences.userPreference
 import com.maksimowiczm.foodyou.core.ui.ArrowBackIconButton
+import com.maksimowiczm.foodyou.core.ui.ext.add
 import com.maksimowiczm.foodyou.feature.barcodescanner.FullScreenCameraBarcodeScanner
 import com.maksimowiczm.foodyou.feature.food.domain.FoodId
 import com.maksimowiczm.foodyou.feature.food.domain.FoodSearch
 import com.maksimowiczm.foodyou.feature.food.preferences.UseOpenFoodFacts
 import com.maksimowiczm.foodyou.feature.food.preferences.UseUSDA
+import com.maksimowiczm.foodyou.feature.food.ui.FoodListItemSkeleton
+import com.maksimowiczm.foodyou.feature.food.ui.search.FoodSearchListItem
 import com.maksimowiczm.foodyou.feature.food.ui.search.FoodSearchView
 import com.maksimowiczm.foodyou.feature.measurement.domain.Measurement
+import com.valentinilk.shimmer.ShimmerBounds
+import com.valentinilk.shimmer.rememberShimmer
 import foodyou.app.generated.resources.*
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -144,6 +163,8 @@ internal fun FoodSearchApp(
         searchState = searchState,
         pages = pages,
         inputField = searchInputField,
+        onFoodClick = onFoodClick,
+        excludes = excludedFood != null,
         modifier = modifier
     )
 }
@@ -265,6 +286,8 @@ private fun FoodSearchApp(
     searchState: SearchBarState,
     pages: LazyPagingItems<FoodSearch>,
     inputField: @Composable () -> Unit,
+    onFoodClick: (FoodSearch, Measurement) -> Unit,
+    excludes: Boolean,
     modifier: Modifier = Modifier
 ) = Scaffold(modifier) { paddingValues ->
     Box(Modifier.focusable().size(1.dp))
@@ -284,6 +307,7 @@ private fun FoodSearchApp(
             .zIndex(10f)
             .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Horizontal))
             .windowInsetsPadding(WindowInsets.displayCutout.only(WindowInsetsSides.Horizontal))
+            .padding(top = paddingValues.calculateTopPadding())
             .onSizeChanged { searchBarHeight = it.height }
             .padding(vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -293,7 +317,6 @@ private fun FoodSearchApp(
             state = searchState,
             inputField = inputField,
             modifier = Modifier
-                .padding(top = paddingValues.calculateTopPadding())
                 .padding(horizontal = 16.dp)
                 .fillMaxWidth(),
             colors = SearchBarDefaults.colors(
@@ -302,4 +325,136 @@ private fun FoodSearchApp(
             shadowElevation = 2.dp
         )
     }
+
+    SearchList(
+        pages = pages,
+        onFoodClick = onFoodClick,
+        contentPadding = contentPadding.add(
+            top = LocalDensity.current.run { searchBarHeight.toDp() },
+            bottom = 56.dp + 32.dp // FAB
+        ),
+        excludes = excludes,
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, FlowPreview::class)
+@Composable
+private fun SearchList(
+    pages: LazyPagingItems<FoodSearch>,
+    onFoodClick: (FoodSearch, Measurement) -> Unit,
+    contentPadding: PaddingValues,
+    excludes: Boolean,
+    modifier: Modifier = Modifier
+) = Box(modifier) {
+    if (pages.itemCount == 0) {
+        Text(
+            text = stringResource(Res.string.neutral_no_food_found),
+            modifier = Modifier
+                .safeContentPadding()
+                .align(Alignment.Center)
+        )
+    }
+
+    if (pages.delayedLoadingState()) {
+        ContainedLoadingIndicator(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = contentPadding.calculateTopPadding())
+                .zIndex(20f)
+        )
+    }
+
+    val shimmer = rememberShimmer(ShimmerBounds.View)
+    LazyColumn(
+        contentPadding = contentPadding
+    ) {
+        if (excludes) {
+            item {
+                Card(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(bottom = 8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+                    )
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = stringResource(
+                                Res.string.description_recipe_missing_ingredients
+                            ),
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+
+        if (pages.loadState.refresh is LoadState.Loading) {
+            items(10) {
+                FoodListItemSkeleton(shimmer)
+            }
+        }
+
+        items(
+            count = pages.itemCount,
+            key = pages.itemKey { it.id.toString() }
+        ) { i ->
+            val food = pages[i]
+
+            when (food) {
+                null -> FoodListItemSkeleton(shimmer)
+                is FoodSearch.Product -> {
+                    val measurement = food.defaultMeasurement
+                    FoodSearchListItem(
+                        food = food,
+                        measurement = measurement,
+                        onClick = { onFoodClick(food, measurement) }
+                    )
+                }
+
+                is FoodSearch.Recipe -> {
+                    val measurement = food.defaultMeasurement
+                    FoodSearchListItem(
+                        food = food,
+                        measurement = measurement,
+                        onClick = { onFoodClick(food, measurement) },
+                        shimmer = shimmer
+                    )
+                }
+            }
+        }
+
+        if (pages.loadState.append is LoadState.Loading) {
+            items(3) {
+                FoodListItemSkeleton(shimmer)
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(FlowPreview::class)
+private fun <T : Any> LazyPagingItems<T>.delayedLoadingState(timeout: Long = 100L): Boolean {
+    var isLoading by remember { mutableStateOf(false) }
+    LaunchedEffect(this) {
+        snapshotFlow {
+            loadState.refresh is LoadState.Loading || loadState.append is LoadState.Loading
+        }
+            .debounce(timeout)
+            .collectLatest { isLoading = it }
+    }
+
+    return isLoading
 }
