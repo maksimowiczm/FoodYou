1b07a0ffee367d484496ea37a1103c5f11a2232d
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2025-08-02 22:35:25 +0200

    add ObserveDailyGoalUseCase and integrate into GoalsViewModel

--- feature3/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/fooddiary/ui/goals/screen/GoalsViewModel.kt
+++ feature3/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/feature/fooddiary/ui/goals/screen/GoalsViewModel.kt
@@ -1,33 +1,16 @@
 package com.maksimowiczm.foodyou.feature.fooddiary.ui.goals.screen
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.ViewModel
-import com.maksimowiczm.foodyou.core.preferences.userPreference
+import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveDailyGoalUseCase
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveMealsUseCase
-import com.maksimowiczm.foodyou.feature.fooddiary.preferences.GoalsPreference
-import kotlinx.coroutines.flow.map
-import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDate
 
 internal class GoalsViewModel(
     private val observeMealsUseCase: ObserveMealsUseCase,
-    dataStore: DataStore<Preferences>
+    private val observeDailyGoalUseCase: ObserveDailyGoalUseCase
 ) : ViewModel() {
-
-    private val goalsPreference = dataStore.userPreference<GoalsPreference>()
 
     fun observeMeals(date: LocalDate) = observeMealsUseCase(date)
 
-    fun observeGoals(date: LocalDate) = goalsPreference.observe().map {
-        when (date.dayOfWeek) {
-            DayOfWeek.MONDAY -> it.monday
-            DayOfWeek.TUESDAY -> it.tuesday
-            DayOfWeek.WEDNESDAY -> it.wednesday
-            DayOfWeek.THURSDAY -> it.thursday
-            DayOfWeek.FRIDAY -> it.friday
-            DayOfWeek.SATURDAY -> it.saturday
-            DayOfWeek.SUNDAY -> it.sunday
-        }
-    }
+    fun observeGoals(date: LocalDate) = observeDailyGoalUseCase.observe(date)
 }
--- feature3/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/infrastructure/di/FoodDiaryModule.kt
+++ feature3/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/infrastructure/di/FoodDiaryModule.kt
@@ -2,6 +2,8 @@ package com.maksimowiczm.foodyou.infrastructure.di
 
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.CreateMeasurementUseCase
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.CreateMeasurementUseCaseImpl
+import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveDailyGoalUseCase
+import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveDailyGoalUseCaseImpl
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveMealsUseCase
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveMealsUseCaseImpl
 import com.maksimowiczm.foodyou.feature.fooddiary.domain.ObserveMeasurementSuggestionsUseCase
@@ -27,4 +29,5 @@ val foodDiaryModule = module {
     viewModelOf(::FoodSearchViewModel)
     factoryOf(::CreateMeasurementUseCaseImpl).bind<CreateMeasurementUseCase>()
     viewModelOf(::GoalsViewModel)
+    factoryOf(::ObserveDailyGoalUseCaseImpl).bind<ObserveDailyGoalUseCase>()
 }
