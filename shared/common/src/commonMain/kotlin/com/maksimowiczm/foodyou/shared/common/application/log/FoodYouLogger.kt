bf68cc9ff0d59621f827dfe5bacdb143b3713bbd
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-09-07 18:17:11 +0200

    bye bye kermit

--- shared/common/src/commonMain/kotlin/com/maksimowiczm/foodyou/shared/common/application/log/FoodYouLogger.kt
+++ shared/common/src/commonMain/kotlin/com/maksimowiczm/foodyou/shared/common/application/log/FoodYouLogger.kt
@@ -2,23 +2,9 @@ package com.maksimowiczm.foodyou.shared.common.application.log
 
 import com.maksimowiczm.foodyou.shared.domain.log.Logger
 
-object FoodYouLogger : Logger {
-
-    private val kermit = co.touchlab.kermit.Logger
-
-    override fun d(tag: String, throwable: Throwable?, message: () -> String) {
-        kermit.d(throwable, tag, message)
-    }
-
-    override fun w(tag: String, throwable: Throwable?, message: () -> String) {
-        kermit.w(throwable, tag, message)
-    }
-
-    override fun e(tag: String, throwable: Throwable?, message: () -> String) {
-        kermit.e(throwable, tag, message)
-    }
-
-    override fun i(tag: String, throwable: Throwable?, message: () -> String) {
-        kermit.i(throwable, tag, message)
-    }
+expect object FoodYouLogger : Logger {
+    override fun d(tag: String, throwable: Throwable?, message: () -> String)
+    override fun w(tag: String, throwable: Throwable?, message: () -> String)
+    override fun e(tag: String, throwable: Throwable?, message: () -> String)
+    override fun i(tag: String, throwable: Throwable?, message: () -> String)
 }
