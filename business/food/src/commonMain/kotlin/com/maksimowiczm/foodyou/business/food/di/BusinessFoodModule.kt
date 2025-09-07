cce0fd9d99588907eda5424f51d6a1a34389ea90
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-09-07 16:22:56 +0200

    clean up

--- business/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/business/food/di/BusinessFoodModule.kt
+++ business/food/src/commonMain/kotlin/com/maksimowiczm/foodyou/business/food/di/BusinessFoodModule.kt
@@ -2,16 +2,13 @@ package com.maksimowiczm.foodyou.business.food.di
 
 import com.maksimowiczm.foodyou.business.food.domain.ExportCsvProductsUseCase
 import com.maksimowiczm.foodyou.business.food.domain.ExportCsvProductsUseCaseImpl
-import com.maksimowiczm.foodyou.business.food.domain.FoodSearchEventHandler
-import com.maksimowiczm.foodyou.business.food.domain.FoodSearchPreferences
 import com.maksimowiczm.foodyou.business.food.domain.FoodSearchUseCase
 import com.maksimowiczm.foodyou.business.food.domain.ImportCsvProductUseCase
 import com.maksimowiczm.foodyou.business.food.domain.ImportCsvProductUseCaseImpl
 import com.maksimowiczm.foodyou.business.food.domain.ImportSwissFoodCompositionDatabaseUseCase
 import com.maksimowiczm.foodyou.business.food.domain.ImportSwissFoodCompositionDatabaseUseCaseImpl
-import com.maksimowiczm.foodyou.business.shared.di.eventHandlerOf
+import com.maksimowiczm.foodyou.business.shared.di.userPreferencesRepository
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.qualifier.qualifier
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -21,15 +18,10 @@ val businessFoodModule = module {
     factoryOf(::ImportSwissFoodCompositionDatabaseUseCaseImpl)
         .bind<ImportSwissFoodCompositionDatabaseUseCase>()
 
-    // TODO
-    //  eventHandlerOf(::FoodDiaryEntryCreatedEventHandler)
-    eventHandlerOf(::FoodSearchEventHandler)
-
     factory {
             FoodSearchUseCase(
                 foodSearchRepository = get(),
-                foodSearchPreferencesRepository =
-                    get(qualifier(FoodSearchPreferences::class.qualifiedName!!)),
+                foodSearchPreferencesRepository = userPreferencesRepository(),
                 foodRemoteMediatorFactoryAggregate = get(),
                 dateProvider = get(),
                 eventBus = get(),
--- business/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/business/fooddiary/di/BusinessFoodDiaryModule.kt
+++ business/fooddiary/src/commonMain/kotlin/com/maksimowiczm/foodyou/business/fooddiary/di/BusinessFoodDiaryModule.kt
@@ -1,11 +1,10 @@
 package com.maksimowiczm.foodyou.business.fooddiary.di
 
 import com.maksimowiczm.foodyou.business.fooddiary.domain.FoodDiaryEntryCreatedEventHandler
-import com.maksimowiczm.foodyou.business.fooddiary.domain.MealsPreferences
 import com.maksimowiczm.foodyou.business.fooddiary.domain.ObserveDiaryMealsUseCase
 import com.maksimowiczm.foodyou.business.fooddiary.domain.ObserveDiaryMealsUseCaseImpl
 import com.maksimowiczm.foodyou.business.shared.di.eventHandlerOf
-import org.koin.core.qualifier.named
+import com.maksimowiczm.foodyou.business.shared.di.userPreferencesRepository
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -13,7 +12,7 @@ val businessFoodDiaryModule = module {
     factory {
             ObserveDiaryMealsUseCaseImpl(
                 mealRepository = get(),
-                mealsPreferencesRepository = get(named(MealsPreferences::class.qualifiedName!!)),
+                mealsPreferencesRepository = userPreferencesRepository(),
                 foodEntryRepository = get(),
                 manualEntryRepository = get(),
                 dateProvider = get(),
