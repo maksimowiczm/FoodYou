4c6b823c309028cc6656d88028660f988c4b892e
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-09-08 11:51:10 +0200

    build logic build.gradle.kts remove group declaration and Java compatibility settings

--- FoodYouCore/build-logic/build.gradle.kts
+++ FoodYouCore/build-logic/build.gradle.kts
@@ -1,12 +1,5 @@
 plugins { `kotlin-dsl` }
 
-group = "com.maksimowiczm.foodyou.core.build_logic"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
-
 dependencies {
     compileOnly(libs.gradle.android)
     compileOnly(libs.gradle.kotlin)
