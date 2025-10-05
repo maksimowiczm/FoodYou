c75cfdcda36361cbec2fafd810898629abab20be
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-10-05 10:02:50 +0200

    refactor: update `DataStore` creation process

--- app/src/androidMain/kotlin/com/maksimowiczm/foodyou/common/infrastructure/datastore/DataStoreModule.android.kt
+++ app/src/androidMain/kotlin/com/maksimowiczm/foodyou/common/infrastructure/datastore/DataStoreModule.android.kt
@@ -1,13 +1,7 @@
 package com.maksimowiczm.foodyou.common.infrastructure.datastore
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import okio.Path.Companion.toPath
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.scope.Scope
 
-internal actual fun Scope.createDataStore(): DataStore<Preferences> =
-    PreferenceDataStoreFactory.createWithPath {
-        androidContext().filesDir.resolve(DATASTORE_FILE_NAME).absolutePath.toPath()
-    }
+internal actual fun Scope.produceDataStoreFile(): String =
+    androidContext().filesDir.resolve(DATASTORE_FILE_NAME).absolutePath
--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/common/infrastructure/datastore/DataStoreModule.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/common/infrastructure/datastore/DataStoreModule.kt
@@ -1,14 +1,14 @@
 package com.maksimowiczm.foodyou.common.infrastructure.datastore
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import okio.Path.Companion.toPath
 import org.koin.core.module.Module
 import org.koin.core.scope.Scope
 
 internal const val DATASTORE_FILE_NAME = "user_preferences.preferences_pb"
 
-internal expect fun Scope.createDataStore(): DataStore<Preferences>
+internal expect fun Scope.produceDataStoreFile(): String
 
 fun Module.dataStoreModule() {
-    single { createDataStore() }
+    single { PreferenceDataStoreFactory.createWithPath { produceDataStoreFile().toPath() } }
 }
