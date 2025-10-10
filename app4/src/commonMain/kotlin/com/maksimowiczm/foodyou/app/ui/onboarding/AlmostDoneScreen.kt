d3f0568ed2bc9df793b530cc31cca2cb00fa6a0e
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-10-10 12:18:28 +0200

    feat: implement onboarding `Almost done` screen

---
+++ app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/ui/onboarding/AlmostDoneScreen.kt
@@ -0,0 +1,84 @@
+package com.maksimowiczm.foodyou.app.ui.onboarding
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.LinearWavyProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.maksimowiczm.foodyou.app.ui.common.component.ReadYouImage
+import com.maksimowiczm.foodyou.app.ui.common.theme.PreviewFoodYouTheme
+import foodyou.app.generated.resources.*
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AlmostDoneScreen(modifier: Modifier = Modifier) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(Res.string.headline_almost_done),
+                        style = MaterialTheme.typography.displaySmall,
+                    )
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            modifier =
+                Modifier.fillMaxSize()
+                    .nestedScroll(scrollBehavior.nestedScrollConnection)
+                    .padding(paddingValues)
+                    .consumeWindowInsets(paddingValues)
+                    .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        ) {
+            ReadYouImage(
+                Modifier.padding(32.dp).sizeIn(maxWidth = 400.dp, maxHeight = 350.dp).fillMaxWidth()
+            )
+            Text(
+                text = stringResource(Res.string.headline_preparing_your_food_diary),
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
+            )
+            LinearWavyProgressIndicator(Modifier.fillMaxWidth())
+            Text(
+                text = stringResource(Res.string.headline_please_wait),
+                modifier = Modifier.padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AlmostDoneScreenPreview() {
+    PreviewFoodYouTheme { AlmostDoneScreen() }
+}
