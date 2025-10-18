2db157347142031b11f13732036293806b4e8ff0
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-10-18 20:58:59 +0200

    feat: enhance navigation transitions with scale animations

--- app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/navigation/ForwardBackwardComposable.kt
+++ app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/navigation/ForwardBackwardComposable.kt
@@ -5,6 +5,9 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.material3.MotionScheme
 import androidx.compose.runtime.*
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
@@ -14,6 +17,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import kotlin.jvm.JvmSuppressWildcards
 import kotlin.reflect.KType
+import kotlin.text.toInt
 
 /**
  * A composable that animates forward and backward navigation with shared axis X transitions.
@@ -115,7 +119,7 @@ fun NavGraphBuilder.forwardBackwardComposable(
 }
 
 object ForwardBackwardComposableDefaults {
-    const val INITIAL_OFFSET_FACTOR = 0.10f
+    const val INITIAL_OFFSET_FACTOR = 0.15f
 
     fun enterTransition(offset: Float = INITIAL_OFFSET_FACTOR) =
         materialSharedAxisXIn(initialOffsetX = { (it * offset).toInt() })
@@ -124,8 +128,14 @@ object ForwardBackwardComposableDefaults {
         materialSharedAxisXOut(targetOffsetX = { -(it * offset).toInt() })
 
     fun popEnterTransition(offset: Float = INITIAL_OFFSET_FACTOR) =
-        materialSharedAxisXIn(initialOffsetX = { -(it * offset).toInt() })
+        scaleIn(
+            animationSpec = MotionScheme.expressive().defaultSpatialSpec(),
+            initialScale = 0.9f,
+        ) + materialSharedAxisXIn(initialOffsetX = { -(it * offset).toInt() })
 
     fun popExitTransition(offset: Float = INITIAL_OFFSET_FACTOR) =
-        materialSharedAxisXOut(targetOffsetX = { (it * offset).toInt() })
+        scaleOut(
+            animationSpec = MotionScheme.expressive().defaultSpatialSpec(),
+            targetScale = 0.9f,
+        ) + materialSharedAxisXOut(targetOffsetX = { (it * offset).toInt() })
 }
