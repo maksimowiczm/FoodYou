e7e35e2d8fc8c027b2096552654a4d904dbef521
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-11-09 17:22:26 +0100

    change FoodRecipeIngredientDto to a sealed interface with Product and Recipe classes

--- app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/food/domain/FoodRecipeIngredientDto.kt
+++ app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/food/domain/FoodRecipeIngredientDto.kt
@@ -3,13 +3,23 @@ package com.maksimowiczm.foodyou.food.domain
 import com.maksimowiczm.foodyou.common.domain.AbsoluteQuantity
 import com.maksimowiczm.foodyou.common.domain.Quantity
 
-class FoodRecipeIngredientDto(
-    val identity: FoodProductIdentity,
-    val name: FoodName,
-    val brand: FoodBrand?,
-    val image: FoodImage?,
-    val servingQuantity: AbsoluteQuantity?,
-    val packageQuantity: AbsoluteQuantity?,
-    val per100gNutritionFacts: NutritionFacts,
-    val quantity: Quantity,
-)
+sealed interface FoodRecipeIngredientDto {
+    class Product(
+        val identity: FoodProductIdentity,
+        val name: FoodName,
+        val brand: FoodBrand?,
+        val image: FoodImage?,
+        val servingQuantity: AbsoluteQuantity?,
+        val packageQuantity: AbsoluteQuantity?,
+        val per100gNutritionFacts: NutritionFacts,
+        val quantity: Quantity,
+    ) : FoodRecipeIngredientDto
+
+    class Recipe(
+        val identity: LocalFoodRecipeIdentity,
+        val name: FoodName,
+        val image: FoodImage?,
+        val per100gNutritionFacts: NutritionFacts,
+        val quantity: Quantity,
+    ) : FoodRecipeIngredientDto
+}
