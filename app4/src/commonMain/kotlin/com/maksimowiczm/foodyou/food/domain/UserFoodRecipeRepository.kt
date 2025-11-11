2285c90afe5b8d741bb539269942707cb0d67dbd
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-11-11 13:06:59 +0100

    add UserFoodRecipeRepository interface

---
+++ app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/food/domain/UserFoodRecipeRepository.kt
@@ -0,0 +1,31 @@
+package com.maksimowiczm.foodyou.food.domain
+
+import com.maksimowiczm.foodyou.common.domain.Quantity
+import kotlinx.coroutines.flow.Flow
+
+interface UserFoodRecipeRepository {
+    suspend fun create(
+        name: FoodName,
+        note: FoodNote?,
+        image: FoodImage?,
+        source: FoodSource?,
+        isLiquid: Boolean,
+        servings: Int,
+        ingredients: List<Pair<FoodIdentity, Quantity>>,
+    ): LocalFoodRecipeIdentity
+
+    suspend fun edit(
+        identity: LocalFoodRecipeIdentity,
+        name: FoodName,
+        note: FoodNote?,
+        image: FoodImage?,
+        source: FoodSource?,
+        isLiquid: Boolean,
+        servings: Int,
+        ingredients: List<Pair<FoodIdentity, Quantity>>,
+    )
+
+    fun observe(identity: LocalFoodRecipeIdentity): Flow<FoodRecipeDto?>
+
+    suspend fun delete(identity: LocalFoodRecipeIdentity)
+}
