83b8fd1d3ee0eeb5874c5150c8044669c6a7072c
mateusz <41268878+maksimowiczm@users.noreply.github.com>
2025-11-11 13:03:08 +0100

    simplify FoodRecipeIngredientDto

--- app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/food/domain/FoodRecipeIngredientDto.kt
+++ app4/src/commonMain/kotlin/com/maksimowiczm/foodyou/food/domain/FoodRecipeIngredientDto.kt
@@ -1,25 +1,9 @@
 package com.maksimowiczm.foodyou.food.domain
 
-import com.maksimowiczm.foodyou.common.domain.AbsoluteQuantity
 import com.maksimowiczm.foodyou.common.domain.Quantity
 
 sealed interface FoodRecipeIngredientDto {
-    class Product(
-        val identity: FoodProductIdentity,
-        val name: FoodName,
-        val brand: FoodBrand?,
-        val image: FoodImage?,
-        val servingQuantity: AbsoluteQuantity?,
-        val packageQuantity: AbsoluteQuantity?,
-        val per100gNutritionFacts: NutritionFacts,
-        val quantity: Quantity,
-    ) : FoodRecipeIngredientDto
+    class Product(val data: FoodProductDto, val quantity: Quantity) : FoodRecipeIngredientDto
 
-    class Recipe(
-        val identity: LocalFoodRecipeIdentity,
-        val name: FoodName,
-        val image: FoodImage?,
-        val per100gNutritionFacts: NutritionFacts,
-        val quantity: Quantity,
-    ) : FoodRecipeIngredientDto
+    class Recipe(val data: FoodRecipeDto, val quantity: Quantity) : FoodRecipeIngredientDto
 }
