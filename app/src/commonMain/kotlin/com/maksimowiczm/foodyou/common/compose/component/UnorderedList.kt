4c2571a30e8705818849c0ccc96e0697613b8cf2
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-03-07 13:21:24 +0100

    refactor: optimize recomposition in `unorderedList`

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/common/compose/component/UnorderedList.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/common/compose/component/UnorderedList.kt
@@ -5,31 +5,43 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 
 @Composable
-fun unorderedList(items: List<String>, prefix: String = "\u2022\t\t") =
-    unorderedList(*items.toTypedArray(), prefix = prefix)
+fun unorderedList(
+    items: List<String>,
+    prefix: String = "\u2022\t\t",
+    textStyle: TextStyle = LocalTextStyle.current,
+) = unorderedList(*items.toTypedArray(), prefix = prefix, textStyle = textStyle)
 
 @Composable
-fun unorderedList(vararg items: String, prefix: String = "\u2022\t\t"): AnnotatedString {
-    val textStyle = LocalTextStyle.current
+fun unorderedList(
+    vararg items: String,
+    prefix: String = "\u2022\t\t",
+    textStyle: TextStyle = LocalTextStyle.current,
+): AnnotatedString {
     val textMeasurer = rememberTextMeasurer()
-    val bulletStringWidth =
-        remember(textStyle, textMeasurer) {
-            textMeasurer.measure(text = prefix, style = textStyle).size.width
-        }
-    val restLine = with(LocalDensity.current) { bulletStringWidth.toSp() }
-    val paragraphStyle = ParagraphStyle(textIndent = TextIndent(restLine = restLine))
+    val density = LocalDensity.current
 
-    return buildAnnotatedString {
-        items.forEach { text ->
-            withStyle(style = paragraphStyle) {
-                append(prefix)
-                append(text)
+    val paragraphStyle =
+        remember(textStyle, prefix) {
+            val bulletStringWidth =
+                textMeasurer.measure(text = prefix, style = textStyle).size.width
+            val restLine = with(density) { bulletStringWidth.toSp() }
+            ParagraphStyle(textIndent = TextIndent(restLine = restLine))
+        }
+
+    return remember(items, prefix, paragraphStyle) {
+        buildAnnotatedString {
+            items.forEach { text ->
+                withStyle(style = paragraphStyle) {
+                    append(prefix)
+                    append(text)
+                }
             }
         }
     }
