e9f0200836f97f21f0174046d7ac0a7e63b43b30
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-03-07 17:04:33 +0100

    ci: update meal file paths in validation workflow and localization script

--- dev/test-meals-localization.bash
+++ dev/test-meals-localization.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory to scan for meal files (hardcoded)
-MEALS_DIR="app/src/commonMain/composeResources/files/meals"
+MEALS_DIR="composeApp/src/commonMain/composeResources/files/meals"
 
 # Colors for output
 RED='\033[0;31m'
