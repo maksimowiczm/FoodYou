a5b833c59c28da2fba1475ad8d1fc9ab576db778
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-03-08 13:12:08 +0100

    refactor: simplify DataStore file path logic using FileKit
    - Removed platform-specific `produceDataStoreFile` implementations.
    - Updated `dataStoreModule` to use `FileKit.databasesDir` for cross-platform DataStore file path resolution.

--- composeApp/src/androidMain/kotlin/com/maksimowiczm/foodyou/app/infrastructure/datastore/DataStoreModule.android.kt
+++
@@ -1,8 +0,0 @@
-package com.maksimowiczm.foodyou.app.infrastructure.datastore
-
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.scope.Scope
-
-internal actual fun Scope.produceDataStoreFile(): String {
-    return androidContext().filesDir.resolve(DATASTORE_FILE_NAME).absolutePath
-}
--- composeApp/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/infrastructure/datastore/DataStoreModule.kt
+++ composeApp/src/commonMain/kotlin/com/maksimowiczm/foodyou/app/infrastructure/datastore/DataStoreModule.kt
@@ -1,14 +1,19 @@
 package com.maksimowiczm.foodyou.app.infrastructure.datastore
 
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import io.github.vinceglb.filekit.FileKit
+import io.github.vinceglb.filekit.databasesDir
+import io.github.vinceglb.filekit.path
+import io.github.vinceglb.filekit.resolve
 import okio.Path.Companion.toPath
-import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
-internal const val DATASTORE_FILE_NAME = "user_preferences.preferences_pb"
-
-internal expect fun Scope.produceDataStoreFile(): String
+private const val DATASTORE_FILE_NAME = "user_preferences.preferences_pb"
 
 val dataStoreModule = module {
-    single { PreferenceDataStoreFactory.createWithPath { produceDataStoreFile().toPath() } }
+    single {
+        PreferenceDataStoreFactory.createWithPath {
+            FileKit.databasesDir.resolve(DATASTORE_FILE_NAME).path.toPath()
+        }
+    }
 }
