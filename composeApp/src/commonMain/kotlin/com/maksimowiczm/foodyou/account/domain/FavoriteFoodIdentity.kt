ad2016aa7d8b72ba29c5ff17ad6ff37a1b3c8db1
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-05-03 11:54:11 +0200

    fix: convert `FavoriteFoodIdentity` subclasses to data classes

--- composeApp/src/commonMain/kotlin/com/maksimowiczm/foodyou/account/domain/FavoriteFoodIdentity.kt
+++ composeApp/src/commonMain/kotlin/com/maksimowiczm/foodyou/account/domain/FavoriteFoodIdentity.kt
@@ -1,14 +1,13 @@
 package com.maksimowiczm.foodyou.account.domain
 
-import kotlin.jvm.JvmInline
 import kotlin.uuid.Uuid
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface FavoriteFoodIdentity {
-    @Serializable @JvmInline value class FoodDataCentral(val fdcId: Int) : FavoriteFoodIdentity
+    @Serializable data class FoodDataCentral(val fdcId: Int) : FavoriteFoodIdentity
 
-    @Serializable @JvmInline value class OpenFoodFacts(val barcode: String) : FavoriteFoodIdentity
+    @Serializable data class OpenFoodFacts(val barcode: String) : FavoriteFoodIdentity
 
-    @Serializable @JvmInline value class UserProduct(val id: Uuid) : FavoriteFoodIdentity
+    @Serializable data class UserProduct(val id: Uuid) : FavoriteFoodIdentity
 }
