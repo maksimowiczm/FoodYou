bbcf7f1ca0f376690b8dc1ccc02f9e4cda25a9ab
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-06-27 10:27:19 +0200

    refactor: hoist `testFoodYouKoinApplication` to commonMain source

--- composeApp/src/androidHostTest/kotlin/com/maksimowiczm/foodyou/app/testFoodYouKoinApplication.android.kt
+++ composeApp/src/androidHostTest/kotlin/com/maksimowiczm/foodyou/app/testFoodYouKoinApplication.android.kt
@@ -1,8 +1,7 @@
 package com.maksimowiczm.foodyou.app
 
 import kotlinx.coroutines.test.TestScope
-import org.koin.core.KoinApplication
 import org.koin.dsl.KoinAppDeclaration
 
-actual fun TestScope.testFoodYouKoinApplication(config: KoinAppDeclaration?): KoinApplication =
-    unavailable("Skipping integration tests on this platform")
+actual val TestScope.platformConfig: KoinAppDeclaration
+    get() = unavailable("Skipping integration tests on this platform")
--- composeApp/src/commonTest/kotlin/com/maksimowiczm/foodyou/app/testFoodYouKoinApplication.kt
+++ composeApp/src/commonTest/kotlin/com/maksimowiczm/foodyou/app/testFoodYouKoinApplication.kt
@@ -1,12 +1,35 @@
 package com.maksimowiczm.foodyou.app
 
+import com.maksimowiczm.foodyou.app.di.AppModule
+import com.maksimowiczm.foodyou.app.di.initFoodYouKoinApplication
+import com.maksimowiczm.foodyou.app.infrastructure.FoodYouConfig
+import com.maksimowiczm.foodyou.common.di.applicationCoroutineScope
+import com.maksimowiczm.foodyou.common.event.EventBus
+import com.maksimowiczm.foodyou.common.event.InMemoryEventBus
+import com.maksimowiczm.foodyou.common.event.LoggingEventBus
 import kotlinx.coroutines.test.TestScope
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.bind
+import org.koin.dsl.module
 
-expect fun TestScope.testFoodYouKoinApplication(config: KoinAppDeclaration? = null): KoinApplication
+fun TestScope.testFoodYouKoinApplication(config: KoinAppDeclaration? = null): KoinApplication =
+    initFoodYouKoinApplication(
+        appModule = AppModule(foodYouConfig = { single { FoodYouConfig("TEST") } })
+    ) {
+        modules(
+            module {
+                applicationCoroutineScope { backgroundScope }
+                single { LoggingEventBus(get<InMemoryEventBus>(), get()) }.bind<EventBus>()
+            }
+        )
+        platformConfig.invoke(this)
+        config?.invoke(this)
+    }
+
+expect val TestScope.platformConfig: KoinAppDeclaration
 
 suspend inline fun TestScope.runKoin(vararg modules: Module, block: suspend Koin.() -> Unit) {
     val koin = testFoodYouKoinApplication {
