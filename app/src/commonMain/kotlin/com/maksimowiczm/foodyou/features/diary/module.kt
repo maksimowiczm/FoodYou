69840baf83a549382b9ea0d2995c880dcfd2868c
maksimowiczm <41268878+maksimowiczm@users.noreply.github.com>
2026-08-11 17:37:51 +0200

    refactor: remove food diary search feature

--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/features/diary/module.kt
+++
@@ -1,9 +0,0 @@
-package com.maksimowiczm.foodyou.features.diary
-
-import com.maksimowiczm.foodyou.features.diary.search.FoodDiarySearchViewModel
-import org.koin.core.module.Module
-import org.koin.core.module.dsl.viewModelOf
-
-internal fun Module.diary() {
-    viewModelOf(::FoodDiarySearchViewModel)
-}
--- app/src/commonMain/kotlin/com/maksimowiczm/foodyou/features/featuresModule.kt
+++ app/src/commonMain/kotlin/com/maksimowiczm/foodyou/features/featuresModule.kt
@@ -1,6 +1,5 @@
 package com.maksimowiczm.foodyou.features
 
-import com.maksimowiczm.foodyou.features.diary.diary
 import com.maksimowiczm.foodyou.features.fooddatacentral.foodDataCentral
 import com.maksimowiczm.foodyou.features.home.home
 import com.maksimowiczm.foodyou.features.language.language
@@ -15,7 +14,6 @@ import com.maksimowiczm.foodyou.features.userrecipe.userRecipe
 import org.koin.dsl.module
 
 val featuresModule = module {
-    diary()
     onboarding()
     home()
     language()
